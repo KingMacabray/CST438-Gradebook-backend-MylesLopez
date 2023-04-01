13b9a459f7598ef64acab2f4e91bf1a759b69805
Myles Lopez <mylopez@csumb.edu>
2023-04-01 04:18:42 -0700

    Got Spring boot app working completely finally. finalizing assign4

--- cst438_gradebook/src/main/java/com/cst438/domain/CourseRepository.java
+++ cst438_gradebook/src/main/java/com/cst438/domain/CourseRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.query.Param;
 
 public interface CourseRepository extends CrudRepository <Course, Integer> {
  
-	@Query("select c from Course c where c.course.course_id=:course_id")
+	@Query("select c from Course c where c.course_id=:course_id")
 	Course findByCourse_id(@Param("course_id") int course_id);
 }
