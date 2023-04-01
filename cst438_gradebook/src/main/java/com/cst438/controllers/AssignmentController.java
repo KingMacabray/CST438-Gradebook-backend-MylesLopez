dbf980f0ffad99a13511793b8cd8dd9237831684
Myles Lopez <mylopez@csumb.edu>
2023-03-31 22:58:38 -0700

    First working added code for assignment 4. Also altered course Repo.

--- cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/AssignmentController.java
@@ -1,16 +1,63 @@
 package com.cst438.controllers;
 
+
+import java.sql.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class AssignmentController {
+import com.cst438.domain.AssignmentIdRepository;
+import com.cst438.domain.AssignmentListDTO;
+import com.cst438.services.RegistrationService;
+import com.cst438.domain.Assignment;
+import com.cst438.domain.AssignmentAllRepository;
 
+@RestController
+@CrossOrigin(origins = {"http://localhost:3000","http://localhost:3001"})
+public class AssignmentController {
 	
-	@PostMapping("/assignment")
-	public String createNewAssignment()  {
+	@Autowired
+	AssignmentIdRepository assignmentIdRepository;
+	
+	@Autowired
+	AssignmentAllRepository assignmentAllRepository;
+	
+	@Autowired
+	RegistrationService registrationService;
+	
+	
+	@PostMapping("/assignment/{assiname}/{duedate}")
+	public String createNewAssignment(@PathVariable("assiname") String assignmentName, @PathVariable("duedate") Date dDate)  {
+		String email = "dwisneski@csumb.edu";
+		
+		
 		return "assignmentId = 3";
+		
 	}
 	
+	
+	@DeleteMapping("/assignment/{id}")
+	public void deleteAssignment(@PathVariable("id") Integer assignmentId)  {
+		Assignment assignment = assignmentIdRepository.findAssignmentById(assignmentId);
+		
+		
+		
+		
+		//return "assignmentId = 3";
+	}
+	
+	@PatchMapping("/assignment/{id}")
+	public void renameAssignment(@PathVariable("id") Integer assignmentId)  {
+		Assignment assignment = assignmentIdRepository.findAssignmentById(assignmentId);
+		
+		
+		
+		//return assignment;
+	}
 	
 }
--- cst438_gradebook/src/main/java/com/cst438/controllers/EnrollmentController.java
+++ cst438_gradebook/src/main/java/com/cst438/controllers/EnrollmentController.java
@@ -32,8 +32,21 @@ public class EnrollmentController {
 	public EnrollmentDTO addEnrollment(@RequestBody EnrollmentDTO enrollmentDTO) {
 		
 		//TODO  complete this method in homework 4
-		
-		return null;
+		Course c = courseRepository.findByCourse_id(enrollmentDTO.course_id);
+		if (c != null) {
+			Enrollment e = new Enrollment();
+			e.setCourse(c);
+			e.setStudentEmail(enrollmentDTO.studentEmail);
+			e.setStudentName(enrollmentDTO.studentName);
+			Enrollment es = enrollmentRepository.save(e);
+			enrollmentDTO.id = es.getId();
+			return enrollmentDTO;
+		} else {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+					"Course does not exist. " + enrollmentDTO.course_id);
+		}
+
+		//return null;
 		
 	}
 
